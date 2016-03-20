085acb7e2e973224550c2580ef77bf65019859a9
dgrico <dgrico@gmail.com>
2016-03-20 03:13:45 -0300

    Resguardo TP

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -1,26 +1,218 @@
 package ar.fiuba.tdd.template.tp0;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
+//import java.util.regex.Pattern;
+//import java.util.regex.PatternSyntaxException;
 
 public class RegExGenerator {
-    // TODO: Uncomment this field
-    private int maxLength;
+
+    private int maxLength = 10; // longitud maxima de la expresion regular
+    private int total = maxLength;
+    private int many = 5; // longitud maxima de cuantificadores, el "muchos"
+    private int securedCounter = 0; // cantidad de posiciones minimas requeridas
+    private ArrayList<String> lstChars = new ArrayList<>();
+    private ArrayList<String> lstQuantifiers = new ArrayList<>();
+    private String partChain = "";
+    int indexRE; // Indice para recorrer la expresion regular
 
     public RegExGenerator(int maxLength) {
         this.maxLength = maxLength;
     }
 
-    // TODO: Uncomment parameters
+    private boolean isQuantifier(Character character) {
+        if (character == '?' || character == '*' || character == '+') {
+            if (character == '+') {
+                securedCounter++;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void putListQuantifier(String regEx) {
+        if (indexRE < regEx.length()) {
+            if (isQuantifier((regEx.charAt(indexRE)))) {
+                lstQuantifiers.add(Character.toString(regEx.charAt(indexRE)));
+                indexRE++;
+            } else {
+                securedCounter++;
+                lstQuantifiers.add("");
+            }
+        } else {
+            lstQuantifiers.add("");
+            securedCounter++;
+        }
+    }
+
+    // Obtiene los valores de los conjuntos
+    private String getSet(String regEx) {
+        String chain = "";
+        char charRE;
+        while (regEx.charAt(indexRE) != ']') {
+            charRE = regEx.charAt(indexRE);
+            chain = chain.concat(String.valueOf(charRE));
+            indexRE++;
+        }
+        return chain;
+    }
+
+    private void processParser(String regEx) {
+        indexRE = 0;
+        while (indexRE <= regEx.length() - 1) {
+            switch (regEx.charAt(indexRE)) {
+                case '.':
+                    lstChars.add(Character.toString(regEx.charAt(indexRE)));
+                    break;
+                case '\\':
+                    indexRE++;
+                    lstChars.add("\\" + Character.toString(regEx.charAt(indexRE)));
+                    break;
+                case '[':
+                    indexRE++;
+                    lstChars.add(getSet(regEx));
+                    break;
+                default: // es un literal, un caracter especifico
+                    lstChars.add(Character.toString(regEx.charAt(indexRE)));
+                    break;
+            }
+            indexRE++;
+            putListQuantifier(regEx);
+        }
+    }
+
+    private int evaluateQuantifier(String quantifier, int maxRepeat) {
+        Random numRan = new Random();
+        switch (quantifier) {
+            case "?":
+                maxRepeat = numRan.nextInt(2);
+                total = total - maxRepeat;
+                break;
+            case "+":
+                maxRepeat = numRan.nextInt(maxRepeat) + 1;
+                total = total - maxRepeat + 1;
+                break;
+            case "*":
+                maxRepeat = numRan.nextInt(maxRepeat + 1);
+                total = total - maxRepeat;
+                break;
+            default:
+                maxRepeat = 1;
+                break;
+        }
+        return maxRepeat;
+    }
+
+    private int getMaxRepeat(String quantifier) {
+        int maxRepeat;
+
+        if (quantifier.compareTo("") == 0) {
+            maxRepeat = 1;
+        } else {
+            if (many < total) {
+                maxRepeat = many;
+            } else {
+                maxRepeat = total;
+            }
+            maxRepeat = evaluateQuantifier(quantifier, maxRepeat);
+        }
+        return maxRepeat;
+    }
+
+    private String getMaxStingCharacter(String possibleChar, int maxRepeat) {
+        String retS = "";
+        String ascii;
+        int asciiValue;
+        int positionChar;
+        int indRep = 1;
+        Random numRan = new Random();
+
+        while (indRep <= maxRepeat) {
+            if (possibleChar.compareTo(".") == 0) { // es cualquier caracter ascii
+                asciiValue = numRan.nextInt(255) + 1;
+                ascii = String.valueOf(Character.toChars(asciiValue));
+            } else {
+                if (possibleChar.length() == 1) { // es un literal
+                    ascii = possibleChar;
+                } else { // es un conjunto de caracteres
+                    positionChar = numRan.nextInt(possibleChar.length());
+                    ascii = Character.toString(possibleChar.charAt(positionChar));
+                }
+            }
+            retS = retS.concat(ascii);
+            indRep++;
+        }
+        return retS;
+    }
+
+    private String getMaxString(String possibleChar, int maxRepeat) {
+        String ascii;
+        String retS = "";
+
+        if (possibleChar.length() == 2) { // es un escape
+            ascii = Character.toString(possibleChar.charAt(possibleChar.length() - 1));
+            retS = retS.concat(ascii);
+        } else {
+            retS = getMaxStingCharacter(possibleChar, maxRepeat);
+/*
+            while (indRep <= maxRepeat) {
+                if (possibleChar.compareTo(".") == 0) { // es cualquier caracter ascii
+                    asciiValue = numRan.nextInt(255) + 1;
+                    ascii = String.valueOf(Character.toChars(asciiValue));
+                } else {
+                    if (possibleChar.length() == 1) { // es un literal
+                        ascii = possibleChar;
+                    } else { // es un conjunto de caracteres
+                        positionChar = numRan.nextInt(possibleChar.length());
+                        ascii = Character.toString(possibleChar.charAt(positionChar));
+                    }
+                }
+                retS = retS.concat(ascii);
+                indRep++;
+            }
+*/
+        }
+        return  retS;
+    }
+
+    private String getChain(String possibleChar, String quantifier) {
+        int maxRepeat;
+        String retS;
+
+        maxRepeat = getMaxRepeat(quantifier);
+        retS = getMaxString(possibleChar, maxRepeat);
+        return retS;
+    }
+
     public List<String> generate(String regEx, int numberOfResults) {
+/*
+        try {
+            Pattern pattern = Pattern.compile("^" + regEx + "$");
+            //System.out.println("Pattern created: "+pattern.pattern());
+        } catch (PatternSyntaxException errorExpression) {
+            System.out.println("This string could not compile: " + errorExpression.getPattern());
+            System.out.println(errorExpression.getMessage());
+            //System.exit(0);
+        }
+*/
         return new ArrayList<String>() {
             {
-                add("abc");
-                add("bac");
-                add("cab");
+                processParser(regEx);
+                total = total - securedCounter;
+
+                for (int i = 1; i <= numberOfResults; i++) {
+                    partChain = "";
+                    Iterator<String> charsIterator = lstChars.iterator();
+                    Iterator<String> quantityIterator = lstQuantifiers.iterator();
+                    while (charsIterator.hasNext()) {
+                        partChain = partChain + getChain(charsIterator.next(), quantityIterator.next());
+                    }
+                    add(partChain);
+                }
             }
         };
-
-
     }
 }
--- src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
+++ src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
@@ -6,17 +6,18 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class RegExGeneratorTest {
 
     private boolean validate(String regEx, int numberOfResults) {
         RegExGenerator generator = new RegExGenerator(numberOfResults);
-        // TODO: Uncomment parameters
         List<String> results = generator.generate(regEx, numberOfResults);
         System.out.println(results);
         // force matching the beginning and the end of the strings
         Pattern pattern = Pattern.compile("^" + regEx + "$");
+
         return results
                 .stream()
                 .reduce(true,
@@ -27,8 +28,6 @@ public class RegExGeneratorTest {
                     (item1, item2) -> item1 && item2);
     }
 
-    //TODO: Uncomment these tests
-
     @Test
     public void testAnyCharacter() {
         assertTrue(validate(".", 1));
@@ -36,9 +35,9 @@ public class RegExGeneratorTest {
 
     @Test
     public void testMultipleCharacters() {
-        assertTrue(validate("...", 1));
+        assertTrue(validate("...", 3));
     }
-/*
+
     @Test
     public void testLiteral() {
         assertTrue(validate("\\@", 1));
@@ -55,14 +54,98 @@ public class RegExGeneratorTest {
     }
 
     @Test
+    public void testZeroOrManyCharacter() {
+        assertTrue(validate("\\@.h*", 1));
+    }
+
+    @Test
+    public void testExplamplePDF() {
+        assertTrue(validate("..+[ab]*d?c", 1));
+    }
+
+    @Test
+    public void testExplamplePDFPositionObligatory() {
+        assertTrue(validate("..+[ab]+d+c", 1));
+    }
+
+    @Test
+    public void testExplamplePDFAsterisk() {
+        assertTrue(validate("..*[ab]*d*c", 1));
+    }
+
+    @Test
     public void testCharacterSet() {
         assertTrue(validate("[abc]", 1));
     }
 
     @Test
-    public void testCharacterSetWithQuantifiers() {
+    public void testCharacterSetWithQuantifierMore() {
         assertTrue(validate("[abc]+", 1));
     }
-    */
-    // TODO: Add more tests!!!
+
+    @Test
+    public void testCharacterSetWithQuantifierAsterisk() {
+        assertTrue(validate("[abc]*", 1));
+    }
+
+    @Test
+    public void testCharacterSetWithQuantifierQuestion() {
+        assertTrue(validate("[abc]?", 1));
+    }
+
+    @Test
+    public void testNumerics() {
+        assertTrue(validate("1*2*3*4*5*6*7*8*9*0*", 1));
+    }
+
+    @Test
+    public void testOneNumber() {
+        assertTrue(validate("[0123456789]", 1));
+    }
+
+    @Test
+    public void testVariarity() {
+        assertTrue(validate("[ABCDEFGHIJKLNMÑOPQRSTUVWXYZ][0123456789][abcdefghijklmnñopqrstuvwxyz]", 10));
+    }
+
+    @Test
+    public void testAnyone() {
+        assertTrue(validate(".*.?.+", 1));
+    }
+
+    @Test
+    public void testPatent() {
+        assertTrue(validate("[ABCDEFGHI][JKLNMOPQ][RSTUVWXYZ] [0123456789][0123456789][0123456789]", 100));
+    }
+
+    @Test
+    public void testDirectionMail() {
+        assertTrue(validate("[abcdefghijklmnñopqrstuvwxyz]+@[abcdefghijklmnñopqrstuvwxyz]+\\.com", 1));
+    }
+
+    @Test
+    public void testBar() {
+        assertTrue(validate("\\. es un punto", 1));
+    }
+
+    @Test
+    public void testSpecialCharacter() {
+        assertTrue(validate("\\?\\+\\* son especiales", 1));
+    }
+/*
+    @Test
+    public void testSpecialCharactera() {
+        assertTrue(validate("\\a", 1));
+    }
+
+    @Test
+    public void testUnsupported() {
+        assertFalse(validate("\\d", 1));
+    }
+
+    @Test
+    public void testUnclosedGorup() {
+        assertTrue(validate("[[]", 1));
+    }
+*/
 }
